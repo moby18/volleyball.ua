506e27f74997976f61f309f761b4b3bd2934f01d
Andrey <moby.ekreative@gmail.com>
2015-02-27 00:18:13 +0200

    add resize carousel + add new symfony2.6 forms

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,6 +29,7 @@ class AppKernel extends Kernel
             new Volley\StatBundle\VolleyStatBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Volley\WebBundle\VolleyWebBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
