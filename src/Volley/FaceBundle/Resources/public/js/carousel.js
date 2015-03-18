5fbd24d3e15ce1ea8a064f3536170d1cc16c8cb2
Andrey <moby.ekreative@gmail.com>
2015-03-18 23:26:32 +0200

    make home page slider

--- src/Volley/FaceBundle/Resources/public/js/carousel.js
+++ src/Volley/FaceBundle/Resources/public/js/carousel.js
@@ -26,6 +26,6 @@ $(document).ready(function(){
 $(window).load(function() {
     var boxheight = $('#myCarousel .carousel-inner').innerHeight();
     var itemlength = $('#myCarousel .item').length;
-    var triggerheight = Math.round(boxheight/itemlength+1);
+    var triggerheight = Math.round(boxheight/itemlength);
     $('#myCarousel .list-group-item').outerHeight(triggerheight);
 });
