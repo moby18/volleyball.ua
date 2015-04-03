8578d6ec8767fa00c0f2c6833b05f9ff2c852f4e
Andrey <moby.ekreative@gmail.com>
2015-04-03 20:22:35 +0300

    change post template + social button

--- src/Volley/WebBundle/Controller/DefaultController.php
+++ src/Volley/WebBundle/Controller/DefaultController.php
@@ -9,22 +9,5 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
-    /**
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @Route("/", name="volley_web_homepage")
-     * @Template()
-     */
-    public function indexAction(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-        // slides
-        $slides = $em->getRepository('VolleyFaceBundle:Slide')->findAll();
-        // news
-        $news = $em->getRepository('VolleyFaceBundle:Post')->findBy(array('category' => 1), array('id' => 'DESC'));
-        return $this->render('VolleyWebBundle:Default:index.html.twig', [
-            'slides' => $slides,
-            'news' => $news
-        ]);
-    }
+
 }
