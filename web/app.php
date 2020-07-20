734ed8beb4019707b7b1007bd3bcd2e23eb22cc9
Andrii Petrash <andrii.petrash@gmail.com>
2020-07-20 14:25:40 +0300

    comment code for prevent indexing old pages volleyball.in.ua

--- web/app.php
+++ web/app.php
@@ -3,15 +3,15 @@
 use Symfony\Component\HttpFoundation\Request;
 
 // prevent bot to access to old urls
-$regex_arr  = ["/catid=/", "/format=/", "/cntr=/", "/option=/", "/start=/", "/page__type=/", "/id=/", "/page_type=/", "/f=/", "/emulatemode=/"];
-if ($_SERVER['QUERY_STRING']) {
-	foreach ($regex_arr as $regex) {
-		if(preg_match($regex, $_SERVER['QUERY_STRING'], $match)) {
-			header('HTTP/1.0 403 Forbidden');
-			exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-		}
-	}
-}
+//$regex_arr  = ["/catid=/", "/format=/", "/cntr=/", "/option=/", "/start=/", "/page__type=/", "/id=/", "/page_type=/", "/f=/", "/emulatemode=/"];
+//if ($_SERVER['QUERY_STRING']) {
+//	foreach ($regex_arr as $regex) {
+//		if(preg_match($regex, $_SERVER['QUERY_STRING'], $match)) {
+//			header('HTTP/1.0 403 Forbidden');
+//			exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+//		}
+//	}
+//}
 
 require __DIR__.'/../vendor/autoload.php';
 if (PHP_VERSION_ID < 70000) {
