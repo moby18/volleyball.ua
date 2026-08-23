6586f1ce27158f4705e3a92d6e54353ffbfd720f
Andrii Petrash <andrii.petrash@gmail.com>
2026-08-23 10:25:41 +0300

    feat: add /admin/posts-chart-data JSON endpoint

--- src/Volley/FaceBundle/Controller/AdminController.php
+++ src/Volley/FaceBundle/Controller/AdminController.php
@@ -3,11 +3,38 @@
 namespace Volley\FaceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Volley\FaceBundle\Service\PostFrequencyCalculator;
 
 class AdminController extends Controller
 {
     public function indexAction()
     {
         return $this->render('VolleyFaceBundle:Admin:index.html.twig');
+    }
+
+    public function chartDataAction(Request $request)
+    {
+        $calculator = new PostFrequencyCalculator();
+
+        $period = $request->query->get('period', PostFrequencyCalculator::PERIOD_DAY);
+        if (!$calculator->isValidPeriod($period)) {
+            $period = PostFrequencyCalculator::PERIOD_DAY;
+        }
+
+        $now = new \DateTime();
+        $from = $calculator->getWindowStart($period, $now);
+        $to = $calculator->getWindowEnd($now);
+
+        $repository = $this->getDoctrine()->getRepository('VolleyFaceBundle:Post');
+        $counts = $repository->countGroupedByPeriod($calculator->getSqlDateFormat($period), $from, $to);
+
+        $series = $calculator->buildSeries($period, $counts, $now);
+
+        return new JsonResponse(array(
+            'period' => $period,
+            'series' => $series,
+        ));
     }
 }
